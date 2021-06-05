60200c45850e75e5ac9aa6b47a73e62c020c2e70
KaiAragaki <adamaragaki@gmail.com>
2021-06-04 20:37:25 -0400

    push before branch

--- R/estimate_score.R
+++ R/estimate_score.R
@@ -15,11 +15,8 @@
 
 estimate_score <- function(df, is_affymetrix, tidy) {
 
-    
     # TODO:
-    # Better naming
     # Reimplement sample name bit at the end
-    # Reimpliment msg adding
     # Reimplement affy part
     if(tidy) {
         rownames <- df[, 1]
@@ -29,7 +26,6 @@ estimate_score <- function(df, is_affymetrix, tidy) {
     }
     
     # Sample rank normalization
-    
     df <- df |>  
         apply(2, as.numeric) |> 
         data.matrix()|> 
@@ -44,15 +40,17 @@ estimate_score <- function(df, is_affymetrix, tidy) {
     score.matrix <- matrix(NA_real_, nrow = 2, ncol = ncol(df))
     
     for (i in 1:2) {
-        gene_set <- gene_sets[, i]
-        sig_genes_in_df <- intersect(gene_set, rownames(df))
+        gene_set <- gene_sets[, i, drop = FALSE]
+        sig_genes_in_df <- intersect(unlist(gene_set), rownames(df))
+        
+        message(glue::glue("Gene set: {colnames(gene_set)}",
+                           "# gene set genes in data: {length(sig_genes_in_df)} (out of {nrow(gene_set)})", .sep = "\n"))
         
         if (length(sig_genes_in_df) == 0) { 
             next
         } else {
             ES.vector <- vector(length = ncol(df))
             
-            # Enrichment score
             for (sample in seq_along(1:ncol(df))) {
                 
                 # Arrange genes by INCREASING expression (remember, ranked previously)
@@ -60,33 +58,20 @@ estimate_score <- function(df, is_affymetrix, tidy) {
                 ordered_genes <- df[gene_list, sample]
 
                 hit <- names(ordered_genes) %in% sig_genes_in_df
-
-                # Inverse vector - 1 is 0, 0 is 1.
                 no_hit <- 1 - hit 
 
-                # Unsure why there's an abs here - should all be positive
-                # Too scared to remove it
                 ordered_genes <- abs(ordered_genes)^0.25
-                
-                # Sum of the ranks for which there were hits
+
                 sum.correl <- sum(ordered_genes[hit == 1])
                 
-                # Running sum of non-hits.
-                # Each non-hit incurs a penalty of 1/(# non-sig-genes)
-                # c(0.01, 0.01, 0.02, 0.03, 0.04, 0.04, 0.05...)
                 F0 <- 
                     (no_hit/sum(no_hit)) |> 
                     cumsum()
 
-                # Running sum of the proportion each gene contributes 
-                # (sum should equal 1)
-                # c(0, 0.3, 0, 0, 0, 0.12, 0...)
                 Fn <- 
                     ((hit*ordered_genes)/sum.correl) |> 
                     cumsum()
 
-                # Sum of running ES
-                # c(-0.01, 0.29, 0.28, 0.27, 0.26, 0.38, 0.37...)
                 ES <- (Fn - F0) |> 
                     sum()
                 ES.vector[sample] <- ES
@@ -97,7 +82,6 @@ estimate_score <- function(df, is_affymetrix, tidy) {
 
     score.data <- data.frame(score.matrix)
     names(score.data) <- colnames(df)
-    row.names(score.data) <- gene_set_names
     estimate.score <- apply(score.data, 2, sum)
    
     if (!is_affymetrix){
@@ -108,14 +92,14 @@ estimate_score <- function(df, is_affymetrix, tidy) {
         score.data
     } else {
         # Calculate ESTIMATE-based tumor purity (Affymetrix-specific)
-        convert_row_estimate_score_to_tumor_purity <- function(x) {
+        calculate_tumor_purity <- function(x) {
             stopifnot(is.numeric(x))
             cos(0.6049872018 + 0.0001467884*x)
         }
         
         est.new <- NULL
         for (i in 1:length(estimate.score)) {
-            est_i <- convert_row_estimate_score_to_tumor_purity(estimate.score[i])
+            est_i <- calculate_tumor_purity(estimate.score[i])
             est.new <- rbind(est.new, est_i)
         }
         colnames(est.new) <- c("TumorPurity")
