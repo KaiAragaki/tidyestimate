0c9881ae65a683e317e028b6912864a5b8da9883
KaiAragaki <adamaragaki@gmail.com>
2021-06-07 11:10:19 -0400

    make code codey

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Normalized RNA Expression of 10 TCGA bladder cancer tumors.
 #'
 #' A dataset containing the normalized (by variance stabilizing transformation,
-#' `DESeq2::vst()`) RNAseq expression of 10 randomly sampled tumors from
+#' \code{DESeq2::vst()}) RNAseq expression of 10 randomly sampled tumors from
 #' the bladder cancer cohort of The Cancer Genome Atlas.
 #'
 #' To reduce size of dataset, the genes that expressed in the lowest 20% (taking
