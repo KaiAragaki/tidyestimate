6c8329c3d481c37bf2a6679add932b4094498139
Kai Aragaki <adamaragaki@gmail.com>
2021-06-10 19:22:06 -0400

    add documentation

---
+++ R/tidyestimate.R
@@ -0,0 +1,22 @@
+#' tidyestimate: A modern implementation of the ESTIMATE algorithm
+#'
+#' The tidyestimate is a lightweight, fast, pipe-friendly re-imagination of the
+#' ESTIMATE package. tidyestimate is used to infer tumor purity from
+#' expression data.
+#' 
+#' @section Authors:
+#' Author (tidyestimate):
+#' 
+#' * Kai Aragaki ([ORCID](http://orcid.org/0000-0002-9458-0426)) (author, maintainer)
+#' 
+#' Authors (ESTIMATE):
+#' 
+#' * Kosuke Yoshihara kyoshihara@mdanderson.org (author)
+#' * P. Roebuck proebuck@mdanderson.org (author, copyright holder)
+#' 
+#' @section Reference:
+#' https://www.nature.com/articles/ncomms3612
+#' 
+#' @docType package
+#' @name tidyestimate
+NULL
